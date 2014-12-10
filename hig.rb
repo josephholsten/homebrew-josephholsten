1511b0be887ab289e578e5babde912232133c73a
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-12-09 19:08:35 -0800

    update hig to 0.1.2

--- hig.rb
+++ hig.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Hig < Formula
   homepage 'https://github.com/josephholsten/hig'
   head 'https://github.com/josephholsten/hig.git'
-  version '0.1.0'
+  version '0.1.2'
   url "https://github.com/josephholsten/hig/archive/#{version}.tar.gz"
   sha1 '1f9cab250c8f0b00f96137087f2889aa2f67c6f1'
 
