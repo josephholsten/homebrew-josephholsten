f92ace9389c991aa4ce2542ba3c58778fbcd2cc6
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-12-09 19:54:51 -0800

    hig: update sha

--- hig.rb
+++ hig.rb
@@ -5,7 +5,7 @@ class Hig < Formula
   head 'https://github.com/josephholsten/hig.git'
   version '0.1.5'
   url "https://github.com/josephholsten/hig/archive/#{version}.tar.gz"
-  sha1 '4116471f350c8cac9e57edb86fa6c85b8c6e5fe6'
+  sha1 '70c8111ee1faad2d40e6ba41be5d488280e43864'
 
   def install
     prefix.install Dir['*']
