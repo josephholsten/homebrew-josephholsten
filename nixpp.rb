71daeeac7dee734440ac24c6f3b00ee488d3bc11
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-08-10 11:27:37 -0700

    nixpp: 0.1.1

--- nixpp.rb
+++ nixpp.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Nixpp < Formula
   homepage 'https://github.com/josephholsten/nixpp'
   head 'https://github.com/josephholsten/nixpp.git'
-  version '0.1.0'
+  version '0.1.1'
   url "https://github.com/josephholsten/nixpp/archive/#{version}.tar.gz"
-  sha256 '489718ce9b2ed2c0ad910a4a57e6c74cdaf6851ca3f72d76d88f273918d210e8'
+  sha256 '621bc8f8b29563680df5a73d858c1e53f5ee0856c7e56c6ef72e5e19518f1cca'
 
   def install
     prefix.install Dir['*']
