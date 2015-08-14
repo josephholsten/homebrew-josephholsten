b44a0495ed59fdc57c8b925fe5b76db4233f01bc
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-08-14 13:49:20 -0700

    hig: bump

--- hig.rb
+++ hig.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Hig < Formula
   homepage 'https://github.com/josephholsten/hig'
   head 'https://github.com/josephholsten/hig.git'
-  version '0.1.7'
+  version '0.1.8'
   url "https://github.com/josephholsten/hig/archive/#{version}.tar.gz"
-  sha256 '054bca929aab599386d3756ea8d2e5aecc3e14e74c8a6181c99a575d722a7242'
+  sha256 '0ced55fc86c8e4a812f47b78ce83ede1fd10786f4a57d85bf8e088aea8c8a098'
 
   def install
     prefix.install Dir['*']
