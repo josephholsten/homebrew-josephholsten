e4d1a81b34c5036ee804aa53727a4dbbe9d18490
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2016-10-13 14:48:00 -0700

    hig: 0.4.2

--- hig.rb
+++ hig.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Hig < Formula
   homepage 'https://github.com/josephholsten/hig'
   head 'https://github.com/josephholsten/hig.git'
-  version '0.4.1'
+  version '0.4.2'
   url "https://github.com/josephholsten/hig/archive/v#{version}.tar.gz"
-  # sha256 '
+  sha256 'dda7aceef074071137994ab9403e4f7522dfadab1569c7a16fec5233e19fa98e'
 
   depends_on 'pandoc' => :build
 
