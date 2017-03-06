f90a152cc38283b6f8579ed79cb06f61390380af
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-03-06 11:10:05 -0800

    prince: update checksum

--- prince.rb
+++ prince.rb
@@ -3,7 +3,7 @@ class Prince < Formula
   homepage 'http://www.princexml.com/'
   version '9.0r5'
   url "http://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
-  sha1 '244d4a6e60916659d6bc39d86eec04b0c81ab47e'
+  sha256 '5aab38db48108f5246b2d477a86e533783cf00825c8db89e2dd02d96e2dd9e84'
   def install
     system "echo #{prefix} | ./install.sh"
   end
