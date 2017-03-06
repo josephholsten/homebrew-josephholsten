3a9c8b6095eefacc2914caf3d3c7b92e7762e12e
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-03-06 11:06:17 -0800

    agshoes: update to 1.0.0

--- agshoes.rb
+++ agshoes.rb
@@ -2,80 +2,59 @@ require 'formula'
 
 class Agshoes < Formula
   homepage 'https://github.com/josephholsten/agshoes'
+  version '1.0.0'
+  sha256 '30d777c8d38c55dff1e477f49195fff923524f4c55dfb83e0a2175a89f536fee'
+
   head "#{homepage}.git"
   url "#{homepage}/archive/v#{version}.tar.gz"
-  version '0.1.0'
-  sha256 'e22e4d44a18ea22ab05c1d4a1648fe3adacd27b09005fcc3b4cef408b2b5c0e7'
 
   depends_on 'nixpp'
-
   # agshoes dependencies
-  - coreutils    # gnu tooling, almost entirely for realpath
-  - mackup       # dotfile management
-  - offline-imap # mail sync
-  - vdirsyncer   # card/cal sync
-  - xz           # improved compress/gzip
+  depends_on 'coreutils'    # gnu tooling, almost entirely for realpath
+  depends_on 'mackup'       # dotfile management
+  depends_on 'offline-imap' # mail sync
+  depends_on 'vdirsyncer'   # card/cal sync
+  depends_on 'xz'           # improved compress/gzip
   #  base tools
-  - ctags                # osx ships with bsd, use exhuberant
-  - exiftool             # image metadata tool
-  - fswatch              # file change events
-  - fzf                  # fuzzy finder
-  - jq                   # json maniplation
-  - pass                 # password store
-  - path-extractor       # path extractor
-  - ssh-copy-id          # osx ssh is missing it
-  - the_silver_searcher  # improved grep
-  - watch                # improved while true do; done;
-  - z                    # improved cd
+  depends_on 'ctags'                # osx ships with bsd, use exhuberant
+  depends_on 'exiftool'             # image metadata tool
+  depends_on 'fswatch'              # file change events
+  depends_on 'fzf'                  # fuzzy finder
+  depends_on 'jq'                   # json maniplation
+  depends_on 'pass'                 # password store
+  depends_on 'path-extractor'       # path extractor
+  depends_on 'ssh-copy-id'          # osx ssh is missing it
+  depends_on 'the_silver_searcher'  # improved grep
+  depends_on 'watch'                # improved while true do; done;
+  depends_on 'z'                    # improved cd
+  # clients
+  depends_on 'khal'                 # vcal client
+  depends_on 'khard'                # vcard client
+  depends_on 'mutt'                 # mail client
+  depends_on 'urlview'              # extract urls from emails
   # - jo                   # json output
-  # - khal                 # vcal client
-  # - khard                # vcard client
   # - ledger               # accounting
   # - lynx                 # html to text
-  # - mutt                 # mail client
   # - oath-toolkit         # otp generator
   # - remind               # reminders
   # - speedread
   # - task                 # taskwarrior
-  # - urlview              # extract urls from emails
+  depends_on 'bash-completion'
+  depends_on 'brew-cask-completion'
+  depends_on 'open-completion'
 
-- name: completions
-  homebrew:
-    name: "{{item}}"
-  with_items:
-  - bash-completion
-  - brew-cask-completion
-  - open-completion
-
-- name: base
-  homebrew_cask:
-    name: "{{item}}"
-  with_items:
-  - 1password    # password safe
-  - dropbox      # file sync
-  - firefox      # improved browser
-  - flux         # easy on the eyes
-  - font-hasklig # ligature font, in caskroom/fonts
-  - gpgtools     # mail.app + gpg encryption
-  - iterm2       # terminal
-  # - arq          # backups
-  # - gemini       # dedupe
-  # - osxfuse      # virtual filesystems
-  # - quicksilver  # launcher
-  # - torbrowser   # anon browser
-
-# - gem: name={{ item }}
-#   with_items:
-#   - maid # file processing
-
-# - npm: name={{ item }} global=yes
-#   with_items:
-#   - wunderline # wunderlist cli
-
-#- pip: name={{ item }} executable=/usr/local/bin/pip3.5
-# with_items:
-  # - habitica # habitica cli
-  # - urlscan  # extract urls from emails
+  # depends_on cask: '1password'    # password safe
+  # depends_on cask: 'dropbox'      # file sync
+  # depends_on cask: 'firefox'      # improved browser
+  # depends_on cask: 'flux'         # easy on the eyes
+  # depends_on cask: 'font-hasklig' # ligature font, in caskroom/fonts
+  # depends_on cask: 'gpgtools'     # mail.app + gpg encryption
+  # depends_on cask: 'iterm2'       # terminal
+  # depends_on cask: 'arq'          # backups
+  # depends_on cask: 'gemini'       # dedupe
+  # depends_on cask: 'osxfuse'      # virtual filesystems
+  # depends_on cask: 'quicksilver'  # launcher
+  # depends_on cask: 'torbrowser'   # anon browser
 
   def install
     prefix.install Dir['*']
