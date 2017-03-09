a9c994ecc215b66688d425df721cffbd6c30f627
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-03-09 14:09:50 -0800

    prince: 11.1

--- prince.rb
+++ prince.rb
@@ -1,9 +1,9 @@
 require 'formula'
 class Prince < Formula
   homepage 'http://www.princexml.com/'
-  version '9.0r5'
+  version '11.1'
   url "http://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
-  sha256 '5aab38db48108f5246b2d477a86e533783cf00825c8db89e2dd02d96e2dd9e84'
+  sha256 '5bd703ce9510b29985cfec52036528add2fba0f5c0e158ff2f523116e4231f75'
   def install
     system "echo #{prefix} | ./install.sh"
   end
