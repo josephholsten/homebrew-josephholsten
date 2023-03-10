8d772a079ce1ab7a9f37d0b139b1db8032942abd
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-03-10 15:13:32 -0800

    nixpp: update to 0.3.2

--- nixpp.rb
+++ nixpp.rb
@@ -1,18 +1,22 @@
-require 'formula'
+require "formula"
 
 class Nixpp < Formula
-  homepage 'https://github.com/josephholsten/nixpp'
-  head 'https://github.com/josephholsten/nixpp.git'
-  version '0.2.2'
-  url "https://github.com/josephholsten/nixpp/archive/v#{version}.tar.gz"
-  sha256 'e22e4d44a18ea22ab05c1d4a1648fe3adacd27b09005fcc3b4cef408b2b5c0e7'
+  desc "Shorthands to improved commands"
+  homepage "https://github.com/josephholsten/nixpp"
+  version '0.3.2'
+  url "https://github.com/josephholsten/nixpp/archive/v#{version}tar.gz"
+  sha256 "ac969d7729dc4b11712a6803b023148328fc1a1d7ad7a6f6d2fa60ff6234a114"
+  head "https://github.com/josephholsten/nixpp.git"
 
-  depends_on 'coreutils' # for realpath
-  depends_on 'less'
-  depends_on 'parallel'
-  depends_on 'terminal-notifier'
+  depends_on "pandoc" => :build
+
+  depends_on "coreutils" # for realpath
+  depends_on "less"
+  depends_on "parallel"
+  depends_on "terminal-notifier"
 
   def install
-    prefix.install Dir['*']
+    system "make"
+    prefix.install Dir["*"]
   end
 end
